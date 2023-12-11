1fdd869a1569de04aa409a8cf718de68417009d8
bubi <43218935+EastTemplar@users.noreply.github.com>
2023-12-11 20:39:38 +0300

    feature: input

---
+++ Assets/Scripts/Domain/Inputs/PlayerInput.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+     public float HorizontalMovement { get; private set; }
+     private InputActions _actions;
+
+     private void Awake()
+     {
+          _actions = new InputActions();
+     }
+
+     private void Update()
+     {
+          HorizontalMovement = _actions.Player.HorizontalMovement.ReadValue<float>();
+     }
+
+     private void OnEnable()
+     {
+          _actions.Enable();
+     }
+
+     private void OnDisable()
+     {
+          _actions.Disable();
+     }
+}
